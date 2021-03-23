eb33c822839b2d622961d346b934b4274a5ddeac
OOjeser <50872233+OOjeser@users.noreply.github.com>
2021-03-24 00:30:01 +0500

    удалил мусор

--- Starter/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-foreach(string f in System.IO.Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory)))
-{
-    var proc = System.Diagnostics.Process.Start("WpfApp1.exe", "\"" + f + '\"');
-    //"WpfApp1.exe \"" +f+'\"';
-
-}

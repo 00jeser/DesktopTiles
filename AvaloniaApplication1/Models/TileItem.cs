05d4cfa9529b5171c4b5f2c359508c0f2b5c4908
OOjeser <boss.gavrilenko@bk.ru>
2022-06-11 23:30:09 +0500

    Styling

--- AvaloniaApplication1/Models/TileItem.cs
+++ AvaloniaApplication1/Models/TileItem.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Avalonia;
+using Avalonia.Media;
 using Avalonia.Media.Imaging;
 using Avalonia.Platform;
 using DesktopTiles.Utils;
@@ -23,8 +24,17 @@ namespace DesktopTiles.Models
             set => this.RaiseAndSetIfChanged(ref icon, value);
         }
 
+        private SolidColorBrush mainColorBrush;
+
+        public SolidColorBrush MainColorBrush
+        {
+            get => mainColorBrush;
+            set => this.RaiseAndSetIfChanged(ref mainColorBrush, value);
+        }
+
         public TileItem(string p)
         {
+            MainColorBrush = new SolidColorBrush(Colors.Transparent);
             base.Name = p.Split('\\').Last();
             base.Path = p;
             if (Name.EndsWith(".lnk"))
@@ -41,11 +51,13 @@ namespace DesktopTiles.Models
         private async void SetIcon(string p)
         {
             Icon = await TileUtil.GetIcon(p);
+            MainColorBrush = new SolidColorBrush(await TileUtil.GetMainColor(p));
         }
         private async void SetURLIcon(string p)
         {
             var IconPath = File.ReadAllLines(p).FirstOrDefault(x => x.StartsWith("IconFile")).Split("=")[1];
             Icon = await TileUtil.GetIcon(IconPath);
+            MainColorBrush = new SolidColorBrush(await TileUtil.GetMainColor(IconPath));
         }
     }
 }
--- AvaloniaApplication1/Utils/TileUtil.cs
+++ AvaloniaApplication1/Utils/TileUtil.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Avalonia.Media;
 using Avalonia.Media.Imaging;
 using DesktopTiles.Models;
 using IWshRuntimeLibrary;
@@ -60,6 +61,78 @@ namespace DesktopTiles.Utils
             }
 
             return AvIrBitmap;
+        }
+
+        public static async Task<Color> GetMainColor(string path)
+        {
+            if (!File.Exists(path))
+                return Colors.Transparent;
+            Dictionary<int, int> colorsCounts = new();
+
+            int SatrationCount = 0, BrightnessCount = 0;
+            float SatrationSumm = 0, BrightnessSumm = 0;
+
+            await Task.Run(() =>
+                {
+                    var fi = System.Drawing.Icon.ExtractAssociatedIcon(path).ToBitmap();
+                    for (int i = 0; i < fi.Width; i++)
+                    {
+                        for (int j = 0; j < fi.Height; j++)
+                        {
+                            if(i <= fi.Width / 5 * 2 && j >= fi.Height / 5 * 2)
+                                continue;
+                            var c = fi.GetPixel(i, j);
+                            //c = System.Drawing.Color.FromArgb((byte)(c.R & 0b11111110), (byte)(c.G & 0b11111110), (byte)(c.B & 0b11111110));
+                            if (c.A == byte.MaxValue)
+                            {
+                                int cH = (int)c.GetHue();
+                                SatrationCount++;
+                                BrightnessCount++;
+                                SatrationSumm += c.GetSaturation();
+                                BrightnessSumm += c.GetBrightness();
+                                if (colorsCounts.ContainsKey(cH))
+                                {
+                                    colorsCounts[cH]++;
+                                }
+                                else
+                                {
+                                    colorsCounts[cH] = 1;
+                                }
+                            }
+                        }
+                    }
+                });
+            if (colorsCounts.Count > 0)
+            {
+                var maxHue = colorsCounts.Aggregate((x, y) => x.Value > y.Value ? x : y).Key;
+                return ColorFromHSV(maxHue, SatrationSumm / SatrationCount, BrightnessSumm / BrightnessCount);
+            }
+
+            return Colors.White;
+        }
+        private static Color ColorFromHSV(double hue, double saturation, double value)
+        {
+            int hi = Convert.ToInt32(Math.Floor(hue / 60)) % 6;
+            double f = hue / 60 - Math.Floor(hue / 60);
+
+            value = value * 255;
+            int v = Convert.ToInt32(value);
+            int p = Convert.ToInt32(value * (1 - saturation));
+            int q = Convert.ToInt32(value * (1 - f * saturation));
+            int t = Convert.ToInt32(value * (1 - (1 - f) * saturation));
+
+            if (hi == 0)
+                return Color.FromArgb(255, (byte)v, (byte)t, (byte)p);
+            else if (hi == 1)
+                return Color.FromArgb(255, (byte)q, (byte)v, (byte)p);
+            else if (hi == 2)
+                return Color.FromArgb(255, (byte)p, (byte)v, (byte)t);
+            else if (hi == 3)
+                return Color.FromArgb(255, (byte)p, (byte)q, (byte)v);
+            else if (hi == 4)
+                return Color.FromArgb(255, (byte)t, (byte)p, (byte)v);
+            else
+                return Color.FromArgb(255, (byte)v, (byte)p, (byte)q);
         }
     }
 }
